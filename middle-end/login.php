089b5603b2ab731b499311ccab6c3c5527e56167
Syed Ali Haider <sh424@njit.edu>
2018-09-13 10:06:29 -0400

    feat: add for njit check

--- middle-end/login.php
+++ middle-end/login.php
@@ -1,52 +1,48 @@
 <?php
 
+
   #Get Post Request From Frontend
+  /*
   $input=file_get_contents('php://input');
   $json=json_decode($input);
+  */
+  $username="username";//$json["login"]["username"];
+  $password="password";//$json["login"]["password"];
 
-  $username=$json["login"]["username"];
-  $password=$json["login"]["password"];
+  $jsonData = array();
+	$jsonData["username"] = "username";
+	$jsonData["password"] = "password";
+	//echo $jsonData["username"] . "\n";
+	//echo $jsonData["password"] . "\n";
+	// Encode the array into JSON.
+	$input = json_encode($jsonData);
 
-  echo $password;
-  echo $username;
-  /*
+
   #Check Against NJIT's login via Post
-  $URL = "";
+
+  $URL = "https://aevitepr2.njit.edu/myhousing/login.cfm";
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL,"https://webauth.njit.edu/idp/profile/SAML2/POST/SSO?execution=e1s1");
+  curl_setopt($ch, CURLOPT_URL, $URL);
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  #curl_setopt($ch, CURLOPT_COOKIESESSION, 1);
-  #curl_setopt($curl, CURLOPT_COOKIEFILE, "");
-  #curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, "j_username=sh424&j_password=toiw0kanr3&_eventId_proceed=");
+  $fields= array(
+    "ucid" => $username,
+    "pass" => $password
+  );
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
 
   $result = curl_exec ($ch);
-  curl_close($ch);
+  //echo $result;
 
-  $njit_url="https://webauth.njit.edu/idp/profile/SAML2/POST/SSO?execution=e1s1";
-  $njit_fields = array("j_username" => "sh424", "j_password" => "toiuw0kanr3", "_eventId_proceed=" => "");;
-  $ch2 = curl_init();
-  curl_setopt($ch2, CURLOPT_URL, $njit_url);
-  curl_setopt($ch2, CURLOPT_POST, 1);
-  curl_setopt($ch2, CURLOPT_POSTFIELDS, http_build_query($njit_fields));
-  curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch2, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch2, CURLOPT_MAXREDIRS, 2);
-  $njit_result = curl_exec($ch2);
-  echo $njit_result;
-  curl_close($ch2);
-
-*/
   #Check Against BackEnd's DB via Post
-  $dbPost=curl("https://web.njit.edu/~jmb37/server.php", $input);
+  $dbPost=curl("https://web.njit.edu/~jmb75/server.php", $input);
 
 
   #TO-DO enrich dbPost with NJIT POST
 
   #Send Results Back to Frontend via Post
-  $frontendPost=curl("https://web.njit.edu/~pr347/front.php", $dbPost);
+  echo $dbPost;
 
 
   #Helper Functions
@@ -56,13 +52,11 @@
     curl_setopt($postRequest, CURLOPT_POSTFIELDS, $content);
     curl_setopt($postRequest, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($postRequest, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/json',
-      'Content-Length: ' . strlen($content))
-    );
-
+      'Content-Type: application/json'
+    ));
     $result = curl_exec($postRequest);
     curl_close($postRequest);
     return $result;
   }
-*/
+
 ?>
