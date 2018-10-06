20cf0dd8e9fb0e6e8494e597dce52deb2aa4c9d5
Syed Ali Haider <sh424@njit.edu>
2018-10-06 15:45:27 -0400

    feat: add autograding

--- middle-end/alpha_login.php
+++ middle-end/alpha_login.php
@@ -15,16 +15,23 @@
     "pass" => $password
   );
 
+  $njitPost = curl("https://aevitepr2.njit.edu/myhousing/login.cfm", $fields, "");
+
+  $njitLogin = False;
+  if(pageTitle($njitPost) == "Please Select a System to Sign Into") {
+    $njitLogin = True;
+  }
 
   #Check Against BackEnd's DB via Post
 
-  $dbPost = curl("https://web.njit.edu/~jmb75/server.php", $input);
+  $dbPost = curl("https://web.njit.edu/~jmb75/server.php", $input, "db");
   $valid = json_decode($dbPost);
   $dbResult = $valid -> isValid;
 
   #Send Results Back to Frontend via Post
 
   $response = array(
+    "njitLogin" => $njitLogin,
     "dbLogin"   => $dbResult
   );
 
@@ -34,18 +41,22 @@
 
 
   #Helper Functions
-  function curl($url, $content) {
+  function curl($url, $content, $type) {
     $postRequest = curl_init($url);
 
     curl_setopt($postRequest, CURLOPT_POST, True);
     curl_setopt($postRequest, CURLOPT_RETURNTRANSFER, True);
     curl_setopt($postRequest, CURLOPT_FOLLOWLOCATION, True);
 
-    curl_setopt($postRequest, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/json'
-    ));
-    curl_setopt($postRequest, CURLOPT_POSTFIELDS, $content);
+    if ($type == "db") {
+      curl_setopt($postRequest, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json'
+      ));
+      curl_setopt($postRequest, CURLOPT_POSTFIELDS, $content);
 
+    } else {
+      curl_setopt($postRequest, CURLOPT_POSTFIELDS, http_build_query($content));
+    }
 
     $result = curl_exec($postRequest);
     curl_close($postRequest);
@@ -53,5 +64,11 @@
     return $result;
   }
 
+  function pageTitle($html) {
+
+    preg_match('/<TITLE>(.*?)<\/TITLE>/', $html, $matches);
+    return $matches[1];
+
+  }
 
 ?>
---
+++ middle-end/autograder.php
@@ -0,0 +1,31 @@
+<?php
+
+  $input = file_get_contents('php://input');
+  $json  = json_decode($input);
+
+  $problem = $json -> {"problem"};
+
+  $answer  = $json -> {"answer"};
+  $correct = "False";
+
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+
+  $result = shell_exec("python 2>&1 - <<EOD
+$problem
+
+EOD");
+
+
+  if($answer == $result) {
+    $correct = "True";
+  }
+
+  $response = array(
+    "correct" => $correct,
+    "result"  => $result
+  );
+
+  print_r(array_values($response));
+
+?>
