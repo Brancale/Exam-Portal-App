834a25235fae0d4579f9b93926d8d08611cf6094
JBrancale <james.brancale@gmail.com>
2018-10-08 15:27:48 -0400

    server update with type field

--- back-end/server.php
+++ back-end/server.php
@@ -1,10 +1,4 @@
  <?php
- 	/*
- 	 * Created by James Brancale
- 	 * 9/9/2018
- 	 * CS490 - Tier 3 - Server/Backend
- 	 */
-
  	// Set content type to JSON
 	header("Content-type: application/json");
 
@@ -14,13 +8,10 @@
 	$userid=$json->{"username"};
 	$userpwd=$json->{"password"};
 
-	//echo "User: " . $userid . "\n";
-	//echo "Pawd: " . $userpwd . "\n";
-
 	// SQL Credentials
 	$servername = "sql2.njit.edu";
 	$username = "jmb75";
-	$password = "ask JMB75";
+	$password = "";
 	$dbname = "jmb75";
 
 	// Create connection
@@ -30,12 +21,12 @@
 	    die("Connection failed: " . $conn->connect_error);
 	}
 
+	// SAMPLE to insert into DB:
+	// INSERT INTO `jmb75`.`USERS` (`ID`, `PWD`, `TYPE`) VALUES ('username', MD5('password'), 1);
+
 	// Query DB
 	$sql = "SELECT ID, PWD FROM USERS WHERE ID = '" . $userid . "'";
 	$result = $conn->query($sql);
-
-	// SAMPLE to insert into DB:
-	// INSERT INTO `jmb75`.`USERS` (`ID`, `PWD`) VALUES ('username', MD5('password'));
 
 	// Local vars for flags and values
 	$recordFound = 0;
@@ -45,22 +36,16 @@
 	// Check resulting records and read.
 	if ($result->num_rows > 0) {
 		$recordFound = 1;
-	    // Output each return row
-	    //while($row = $result->fetch_assoc()) {
-	        //echo "ID: " . $row["ID"]. " - PWD: " . $row["PWD"]. "\n";
-	    //}
 	    $row = $result->fetch_assoc();
 	    $uid = $row["ID"];
 	    $hash = $row["PWD"];
-	} /*elseif ($result->num_rows > 1) {
-		//echo "DB entry error. Too many rows with same key.\n";
-	} else {
-	    //echo "0 results\n";
-	}*/
+	    $type = $row["TYPE"];
+	}
 	// Close DB connection
 	$conn->close();
 
 	$response = array();
+	$response["type"] = $type;
 
 	// If records found, compare password to hash and set isValid flag
 	if ($recordFound == 1) {
@@ -69,13 +54,11 @@
 		    //The JSON data.
 			$response["isValid"] = "true";
 		} else {
-			$response["isValid"] = "error";
+			$response["isValid"] = "false";
 		}
 	} else {
 		$response["isValid"] = "false";
 	}
-	//$response["username"] = $uid;
-	//$response["password"] = $hash;
 
 	// Respond with JSON object
 	$json_response = json_encode($response);
