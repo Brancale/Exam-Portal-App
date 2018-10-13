b159314851b06a9798a7903a64706f3472fa9f92
JBrancale <james.brancale@gmail.com>
2018-10-12 22:16:24 -0400

    update

--- back-end/addExamPHP.php
+++ back-end/addExamPHP.php
@@ -12,7 +12,7 @@
 
 	// Test JSON for adding an Exam
 	$jsonData["operationID"] = '3';
-	// Data format: semicolon separated Question IDs, semicolon separated answers, Open time in milliseconds, close time
+	// Data format: delimeter separated Question IDs, semicolon separated answers, Open time in milliseconds, close time
 	$jsonData["data"] = "('1|*|2','Hello World!|*|1 1 2 3 5',1543640400000,1545368400000)";
 
 	// Encode the array into JSON.
