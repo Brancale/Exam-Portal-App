74afe9cbe8dde4f6c1dbf76976aaa76846b1112e
Syed Ali Haider <syeda.haider97@yahoo.com>
2018-10-14 08:36:24 -0400

    add: sample questions

---
+++ temp.py
@@ -0,0 +1,60 @@
+def isRange(num,left,right):
+    return num >= left and num <= right
+
+print(isRange(5,1,6))
+
+
+def maxArray(lst):
+    maxVal = lst[0]
+    for item in lst:
+        if item > maxVal:
+            maxVal = item
+    return maxVal
+
+print(maxArray([1,6,2,5,12]))
+
+
+def initialVowels(sentence):
+    vowels = "AEIOUaeiou"
+    words = sentence.split()
+    lst = []
+    for word in words:
+        if word[0] in vowels:
+            lst.append(word)
+    return lst
+
+print(initialVowels('Our lives begin to end the day we become silent about things that matter'))
+
+
+def repeatWords(sentence):
+    words = sentence.split()
+    lst = []
+    for word in words:
+        if words.count(word) > 1 and word not in lst:
+            lst.append(word)
+    return lst
+
+print(repeatWords('repeat words repeat word words repeat no repeat'))
+
+
+def evenLengths(sentence):
+    count = 0
+    for word in sentence:
+        if len(word) % 2 == 0:
+            count += 1
+    return count
+print(evenLengths(["hi","bye","cart","mart"]))
+
+
+def beginsWith(letter, sentence):
+    count = 0
+    for word in sentence:
+        if word[0] == letter:
+            count += 1
+    return count
+
+print(beginsWith('t',['the','rain','in','spain','falls','mainly','on','the','plain']))
+
+
+
+
