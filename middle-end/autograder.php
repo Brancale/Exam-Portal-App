ab92463782cdd5f4ef708fa5355603c6cd62f936
Syed Ali Haider <sh424@njit.edu>
2018-11-10 14:45:31 -0500

    fix: grading bugs

--- middle-end/autograder.php
+++ middle-end/autograder.php
@@ -35,26 +35,26 @@
   $expectedCall = $expectedCall[1];
 
   if($functionCall != $expectedCall) {
-    $points = $points - $constraintPoints/(sizeof($constraints) + 1);
+    $points = $points - $constraintPoints/(sizeof($constraints) + 2);
     $response = preg_replace("/$functionCall\(/", $expectedCall . "(", $response);
 
     $feedback = $feedback . "Incorrect Function Name: " . $functionCall . "\n"
                           . "Expected Function Name: " . $expectedCall . "\n"
-                          . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+                          . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
   } else {
     $feedback = $feedback . "Constraint Passed: Valid Function Name: " . $functionCall . "\n"
-                          . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+                          . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
   }
 
   // Return Calls
   if(!preg_match("/return/", $response)) {
-    $points = $points - $constraintPoints/(sizeof($constraints) + 1);
+    $points = $points - $constraintPoints/(sizeof($constraints) + 2);
     $response = str_replace("print(", "return(", $response);
     $feedback = $feedback . "No Function Return" . "\n"
-                          . "Points Deducted: "  . round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+                          . "Points Deducted: "  . round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
   } else {
     $feedback = $feedback . "Constraint Passed: Function Returns a Value" . "\n"
-                          . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+                          . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
   }
 
   // Constraint Checking - for/while loop and if/elif/else
@@ -62,51 +62,51 @@
     switch ($constraint) {
       case 1:
         if(!preg_match("/for.*:/", $response)) {
-          $points = $points - $constraintPoints/(sizeof($constraints) + 1);
+          $points = $points - $constraintPoints/(sizeof($constraints) + 2);
           $feedback = $feedback . "Expected use of 'for' loop" . "\n"
           . "No 'for' loop found" . "\n"
-          . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+          . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
         } else {
           $feedback = $feedback . "Constraint Passed: Function uses 'for' loop" . "\n"
-                                . "Points Awarded: ". $constraintPoints/(sizeof($constraints) + 1) . "\n\n";
+                                . "Points Awarded: ". $constraintPoints/(sizeof($constraints) + 2) . "\n\n";
         }
         break;
 
       case 2:
         if(!preg_match("/if.*:/", $response)) {
-          $points = $points - round($constraintPoints/(sizeof($constraints) + 1), 2);
+          $points = $points - round($constraintPoints/(sizeof($constraints) + 2), 2);
           s;
 
           $feedback = $feedback . "Expected use of Conditional Statements" . "\n"
           . "No 'if', 'elif', or 'else' found" . "\n"
-          . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+          . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
         } else {
           $feedback = $feedback . "Constraint Passed: Function uses conditional blocks" . "\n"
-                                . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+                                . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
         }
         break;
 
         case 3:
           if(!preg_match("/while.*:/", $response)) {
-            $points = $points - $constraintPoints/(sizeof($constraints) + 1);
+            $points = $points - $constraintPoints/(sizeof($constraints) + 2);
             $feedback = $feedback . "Expected use of 'while' loop" . "\n"
             . "No 'while' found" . "\n"
-            . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+            . "Points Deducted: " . round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
           } else {
             $feedback = $feedback . "Constraint Passed: Function uses 'while' loop" . "\n"
-                                  . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+                                  . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
           }
           break;
 
         case 4:
           if(substr_count($response, $expectedCall . "(") < 2) {
-            $points = $points - $constraintPoints/(sizeof($constraints) + 1);
+            $points = $points - $constraintPoints/(sizeof($constraints) + 2);
             $feedback = $feedback . "Expected use of recursion" . "\n"
             . "No recursion used" . "\n"
-            . "Points Deducted: " .  round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+            . "Points Deducted: " .  round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
           } else {
             $feedback = $feedback . "Constraint Passed: Function uses recursion" . "\n"
-                                  . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 1), 2) . "\n\n";
+                                  . "Points Awarded: ". round($constraintPoints/(sizeof($constraints) + 2), 2) . "\n\n";
           }
           break;
     }
@@ -124,8 +124,10 @@ EOD");
 
     if($result != $autogradeAnswers[$i]) {
       $points = $points - round($testCasePoints/sizeof($autogradeItems), 2);
-      if(preg_match("/File \"<stdin>\".*/", $result)) {
-        $result = preg_replace("/File \"<stdin>\", line [0-9]+\n/", "", $result);
+      if(preg_match("/.*File \"<stdin>\".*/", $result)) {
+        $lines = explode("\n", $result);
+        $lines = array_slice($lines, sizeof($lines) - 2);
+        $result = implode("\n", $lines);
       }
       $feedback = $feedback . "Test Case " . ($i+1) . " Failed.\n"
                             . "Expected "  . $autogradeAnswers[$i] . " for " . $autogradeItems[$i] . "\n"
